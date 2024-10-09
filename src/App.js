80a5e464d90d11332a900a9cc267a22b267c5041
Nate Hernandez-Marcellus <nathaniel.marellus@outlook.com>
2024-10-09 19:54:45 -0400

    Fixed contact route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './styles/App.css';
 import Main from './pages/Main';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Main />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
